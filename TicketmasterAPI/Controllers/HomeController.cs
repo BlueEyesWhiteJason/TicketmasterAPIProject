47b1460d17f50e592c653e0283854a4f7e948ec8
Jason Speet <jason.speet@gmail.com>
2019-11-04 22:00:42 -0500

    tried to make API work, didn't really work

--- TicketmasterAPI/Controllers/HomeController.cs
+++ TicketmasterAPI/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using TicketmasterAPI.Models;
 
 namespace TicketmasterAPI.Controllers
@@ -12,8 +15,31 @@ namespace TicketmasterAPI.Controllers
     {
         public IActionResult Index()
         {
+            string eventName = CallDiscoveryAPI();
+            JToken p = ParseJsonString(eventName);
             return View();
         }
+
+        private JToken ParseJsonString(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CallDiscoveryAPI()
+        {
+            string key = "dW7a1zq6RyK4otyVGzTtIQtg6iMU53N1";
+            HttpWebRequest request = WebRequest.CreateHttp($"https://app.ticketmaster.com/discovery/v2/events.json?apikey={key}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //This will convert the response to a string
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string APIText = rd.ReadToEnd();
+
+            return APIText;
+        }
+
+       
 
         public IActionResult Privacy()
         {
