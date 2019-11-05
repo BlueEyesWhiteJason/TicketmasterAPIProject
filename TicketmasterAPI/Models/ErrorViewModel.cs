7bfd6eaf3da6c1581a2abbe30298c9684c521869
Jason Speet <jason.speet@gmail.com>
2019-11-04 20:36:41 -0500

    first commit

---
+++ TicketmasterAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketmasterAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
