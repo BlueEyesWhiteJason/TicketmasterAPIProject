6ad0b67680a5ace326f37a8a872f83c67151e242
Flaka Mahmuti <flakamahmuti@gmail.com>
2019-11-06 14:18:12 -0500

    Added APImethods

--- TicketmasterAPI/Controllers/HomeController.cs
+++ TicketmasterAPI/Controllers/HomeController.cs
@@ -5,11 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TicketmasterAPI.Models;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace TicketmasterAPI.Controllers
 {
     public class HomeController : Controller
     {
+        public string CallEventAPI()
+        {
+            string key = "dW7a1zq6RyK4otyVGzTtIQtg6iMU53N1";
+            HttpWebRequest request = WebRequest.CreateHttp("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=dW7a1zq6RyK4otyVGzTtIQtg6iMU53N1");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APIText = rd.ReadToEnd();
+            return APIText;
+        }
+        public string CallEventDetailsAPI()
+        {
+            string key = "dW7a1zq6RyK4otyVGzTtIQtg6iMU53N1";
+            HttpWebRequest request = WebRequest.CreateHttp("https://app.ticketmaster.com/discovery/v2/events/G5diZfkn0B-bh.json?apikey=dW7a1zq6RyK4otyVGzTtIQtg6iMU53N1");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APIText = rd.ReadToEnd();
+            return APIText;
+
+        }
         public IActionResult Index()
         {
             return View();
