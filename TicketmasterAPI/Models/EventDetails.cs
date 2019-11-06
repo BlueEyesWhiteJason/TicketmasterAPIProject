bc92102eaae8adc2a258296c5b952ee72e98e0d8
Flaka Mahmuti <flakamahmuti@gmail.com>
2019-11-06 14:49:50 -0500

    Model add

---
+++ TicketmasterAPI/Models/EventDetails.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketmasterAPI.Models
+{
+    public class EventDetails
+    {   [Key]
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public double Distance { get; set; }
+        public EventDetails()
+        {
+
+        }
+        public EventDetails(JToken t)
+        {
+            this.Id = int.Parse(t["id"].ToString());
+            this.Name = t["name"].ToString();
+            this.Type = t["type"].ToString();
+            this.Distance = double.Parse(t["distance"].ToString());
+
+        }
+    }
+}
