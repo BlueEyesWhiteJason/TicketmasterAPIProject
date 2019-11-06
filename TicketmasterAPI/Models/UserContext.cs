adc9c5bcce4ecf1c03fcfca1621b375470a37b69
Sam Thomson <samuelthomson62@gmail.com>
2019-11-06 15:43:26 -0500

    Started building out the login system. Note that I have not usedmigrations to create the db yet as I am unsure of if we are using the same db for API data and User login

---
+++ TicketmasterAPI/Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketmasterAPI.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data Source =DESKTOP-0M7F4MC\SQLEXPRESS;Initial Catalog=User;Trusted_Connection=True;");
+            //NOTE! I haven't made migrations yet to build out the db. Unsure if we will use the same db for User and API data.
+        }
+    }
+}
