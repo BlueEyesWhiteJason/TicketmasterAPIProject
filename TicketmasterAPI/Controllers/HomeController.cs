5f13944509c61e5be33073a117045e89ab549828
Sam Thomson <samuelthomson62@gmail.com>
2019-11-07 20:30:06 -0500

    pushing my changes up

--- TicketmasterAPI/Controllers/HomeController.cs
+++ TicketmasterAPI/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -60,30 +60,11 @@ namespace TicketmasterAPI.Controllers
         }
         public IActionResult Index()
         {
-            string eventName = CallDiscoveryAPI();
-            JToken p = ParseJsonString(eventName);
+           
             return View();
         }
 
-        private JToken ParseJsonString(string eventName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string CallDiscoveryAPI()
-        {
-            string key = "dW7a1zq6RyK4otyVGzTtIQtg6iMU53N1";
-            HttpWebRequest request = WebRequest.CreateHttp($"https://app.ticketmaster.com/discovery/v2/events.json?apikey={key}");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            //This will convert the response to a string
-            StreamReader rd = new StreamReader(response.GetResponseStream());
-
-            string APIText = rd.ReadToEnd();
-
-            return APIText;
-        }
-
+            
        
 
         public IActionResult Privacy()
